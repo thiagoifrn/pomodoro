4a58f59f905fb33f0e3313f6206c2ddcd8bb0329
thiagoifrn <thiagoifrn@gmail.com>
2021-08-16 18:29:44 -0300

    Adicionando projeto pomodoro

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/pages/pomodoro.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create:(_) => PomodoroStore(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          
+          primarySwatch: Colors.green,
+        ),
+        home: Pomodoro(),
+      ),
+    );
+  }
+}
+
+
+
